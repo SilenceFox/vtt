7e0d8193c17589c5a4422d35f95756091bb5f423
RefGonalves <hikuroyamiryuu@gmail.com>
2024-05-01 21:17:56 -0300

    Mudanças no CSS
    Mudei algumas coisas importantes no CSS:
    
    - O formulário de username agora está com CSS provisório.
    - A tela de boas-vindas agora está com css provisório.
    - Mudança do nome de algumas classes.

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -3,7 +3,7 @@
 	import { goto } from '$app/navigation';
 	let username: string = '';
 	let showForm: boolean = false;
-    let timeout = 6000;
+    let timeout = 6000 * 1000;
 	onMount(() => {
 		const storedUsername: string | null = localStorage.getItem('username');
 		if (storedUsername) {
@@ -23,13 +23,34 @@
 	}
 </script>
 
-{#if showForm}
-	<form on:submit|preventDefault={handleSubmit}>
-		<label for="username">Username:</label>
-		<input id="username" type="text" bind:value={username} required />
-		<button type="submit">Save</button>
-	</form>
-{:else}
-	<p>Welcome, {username}!</p>
-  <button on:click={() => goto('/chat')}>Go to Home (You will be redirected in {timeout/1000} seconds)</button>
-{/if}
+<div class="container-greet">
+	{#if showForm}
+		<form id="login-form" on:submit|preventDefault={handleSubmit}>
+				<label for="username">
+					<i class="user-icon nf nf-oct-person_fill"></i>
+				</label>
+				<input id="username" type="text" bind:value={username} placeholder="Username..." required />
+			<button class="form-submit" type="submit">
+				<i class="nf nf-fa-play"></i>
+			</button>
+		</form>
+	{:else}
+		<div id="greeting">
+			Welcome,
+			<span>
+				{username}
+			</span>
+		</div>
+		<button on:click={() => goto('/chat')}>
+			<i class="nf nf-fa-play"></i>
+		</button>
+		<span id="timeout-greet">
+			You will be redirected in {timeout/1000} seconds
+		</span>
+	{/if}
+</div>
+
+<style>
+	@import 'https://www.nerdfonts.com/assets/css/webfont.css';
+	@import './styles.css';
+</style>
