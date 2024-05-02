40ae7466eeaacbd09b1ba191803e35d166e552b1
SilenceFox <cristianplaybr@gmail.com>
2024-05-02 05:28:20 -0300

    Included a developer route for testing new features and a component alias for Vite

---
+++ src/routes/dev.svelte
@@ -0,0 +1,30 @@
+<!--- import all the development stuff here to try out --->
+<script lang="ts">
+import { notify } from '../components/util/notification.svelte'; // Adjust the path as necessary
+ function triggerNotification() {
+    notify('This is a notification message!', { duration: 100 });
+ }
+</script>
+<body>
+ <h1>whats up</h1>
+ <button on:click={triggerNotification}> Show Notification</button>
+</body>
+
+
+
+
+
+<!--- Put your fancy stuff here --->
+<style>
+@import './styles.css';
+body {
+ justify-content: center;
+}
+h1 {
+ color: red;
+ font-size: 3rem;
+}
+button {
+ color:white;
+}
+</style>
--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,22 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { webSocketServer } from './webSocket.js';
-
 /** @type {import('vite').UserConfig} */
 const config = {
-	server: {
-		port: 3000
-	},
-	preview: {
-		port: 3000
-	},
-	plugins: [sveltekit(), webSocketServer]
+    server: {
+        port: 3000
+    },
+    preview: {
+        port: 3000
+    },
+    plugins: [sveltekit(), webSocketServer],
+    vite: {
+        resolve: {
+            alias: {
+                // Define your alias here
+                $components: '/src/components',
+            },
+        },
+        // other Vite options...
+    },
 };
-
 export default config;
