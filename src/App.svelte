d57e30bebb58bb7795ea8a12c6cda34b4db6f86a
RefGonalves <hikuroyamiryuu@gmail.com>
2024-05-03 17:37:14 -0300

    HTML+CSS: chat
    Created the basic structured for the chat, while adding some things:
    
    • Basic variables for the theme;
    • Basic chat-container structure (TODO: create a component and make its own .css file)
    • Basic text input form (TODO: make it work)

--- src/App.svelte
+++ src/App.svelte
@@ -2,46 +2,49 @@
   import svelteLogo from './assets/svelte.svg'
   import viteLogo from '/vite.svg'
   import Counter from './lib/Counter.svelte'
+  let username = 'Rafa'
+  let time = '01/01/2024 - 01:00:30'
 </script>
 
-<main>
-  <div>
-    <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-      <img src={viteLogo} class="logo" alt="Vite Logo" />
-    </a>
-    <a href="https://svelte.dev" target="_blank" rel="noreferrer">
-      <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
-    </a>
+<body>
+  <div id="chat-container">
+    <div id="chat-header">
+      <p class="username-show">{username}</p>
+    </div>
+    <!-- NOTE: Esse é o quadrado de cor mais clara onde vão ficar as mensagens dentro. -->
+    <!-- TODO: É tarefa do script deixar o chat na parte debaixo toda vez que uma mensagem é adicionada. -->
+    <div id="chat-content">
+      <!-- NOTE: Aqui tem um exemplo de mensagem do PRÓPRIO usuários. -->
+      <div class="user-chat-message">
+        <div class="message-name">{username}</div>
+        <div class="message-text">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Magni, odit.</div>
+        <div class="message-time">{time}</div>
+      </div>
+      <!-- NOTE: Aqui acaba o exemplo de mensagem do PRÓPRIO usuários. -->
+      <!-- NOTE: Aqui começa o exemplo de mensagem do OUTRO usuário. -->
+      <div class="chat-message">
+        <div class="message-name">{username}-2</div>
+        <div class="message-text">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Minus modi officiis quo sint quae iusto ut minima tenetur repudiandae quaerat sapiente ullam nam repellat distinctio, consequatur dicta ipsam velit doloremque.</div>
+        <div class="message-time">{time}</div>
+      </div>
+      <!-- NOTE: Aqui acaba o exemplo de mensagem do OUTRO usuário. -->
+      <div class="chat-message">
+        <div class="message-name">{username}-2</div>
+        <div class="message-text">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Minus modi officiis quo sint quae iusto ut minima tenetur repudiandae quaerat sapiente ullam nam repellat distinctio, consequatur dicta ipsam velit doloremque.</div>
+        <div class="message-time">{time}</div>
+      </div>
+    </div>
+    <div id="chat-footer">
+      <!-- TODO: Rato vai arrumar isso daqui pra funcionar de verdade. -->
+      <form method="GET">
+        <input id="chat-input" type="text" autocomplete="off" name="chat-input" placeholder="Send a message...">
+        <button id="chat-submit">
+          <i class="nf nf-md-send"></i>
+        </button>
+      </form>
+    </div>
   </div>
-  <h1>Vite + Svelte</h1>
-
-  <div class="card">
-    <Counter />
-  </div>
-
-  <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank" rel="noreferrer">SvelteKit</a>, the official Svelte app framework powered by Vite!
-  </p>
-
-  <p class="read-the-docs">
-    Click on the Vite and Svelte logos to learn more
-  </p>
-</main>
+</body>
 
 <style>
-  .logo {
-    height: 6em;
-    padding: 1.5em;
-    will-change: filter;
-    transition: filter 300ms;
-  }
-  .logo:hover {
-    filter: drop-shadow(0 0 2em #646cffaa);
-  }
-  .logo.svelte:hover {
-    filter: drop-shadow(0 0 2em #ff3e00aa);
-  }
-  .read-the-docs {
-    color: #888;
-  }
 </style>
